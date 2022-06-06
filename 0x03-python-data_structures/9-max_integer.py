bfa21d8c50187edd4f219c3a21ab9da10f43741a
tsinatwon <tsinat2wondimu@gmail.com>
2022-06-07 00:21:14 +0300

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    max_val = my_list[0]
+    if len(my_list) == 0:
         return None
-    max_val = 0
     for i in my_list:
         if i > max_val:
             max_val = i
+        else:
+            continue
     return max_val
