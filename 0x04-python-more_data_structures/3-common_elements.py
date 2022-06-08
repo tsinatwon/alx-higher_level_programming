820e16b8632a87c9e1c2cae9bb801d3f4f7597b3
tsinatwon <tsinat2wondimu@gmail.com>
2022-06-08 22:48:53 +0000

    keys and value

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+
+
+def common_elements(set_1, set_2):
+    """return set of common elements in 2 sets"""
+    return (set_1 & set_2)
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+
+
+def number_keys(a_dictionary):
+    """Return the amount of keys in  dictionaries."""
+    return (len(a_dictionary))
+
