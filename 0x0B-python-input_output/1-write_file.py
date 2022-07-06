97a64fc4abbdc9a729fe473370acb61297f47976
tsinatwon <tsinat2wondimu@gmail.com>
2022-07-06 09:53:32 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-"""
-    2-append_write: append_write()
-"""
+# 3-write_file.py
 
 
-def append_write(filename="", text=""):
-    """
-        append_write appends a string at the end of a text file.
-        Args:
-            filename (str): name of file.
-            text (str): text to be appended to the file.
-        Returns: number of characters written.
-    """
-    with open(filename, "a", encoding='utf=8') as a_file:
-        return a_file.write(text)
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file."""
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
