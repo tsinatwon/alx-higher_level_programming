be4e040be8e168f813615c3b6d821ae93366961c
tsinatwon <tsinat2wondimu@gmail.com>
2022-07-13 13:02:18 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,11 +6,13 @@ def safe_print_list(my_list=[], x=0):
      prints possible numer of elements
     """
     try:
+        count = 0
         for i in range(x):
+            count += 1
             print("{}".format(my_list[i]), end="")
         else:
             print()
-            return (i + 1)
+            return (count)
     except IndexError:
         print("")
-        return (i)
+        return (count - 1)
