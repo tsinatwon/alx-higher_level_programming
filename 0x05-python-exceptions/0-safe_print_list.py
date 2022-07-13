808023d2a024a0aa3e868ebcc16c3be8247c1158
tsinatwon <tsinat2wondimu@gmail.com>
2022-07-13 12:22:09 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
         else:
-            print('\n')
+            print()
             return (i + 1)
     except IndexError:
         print("")
