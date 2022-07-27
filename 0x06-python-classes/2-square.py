5fb8ffbd8fcceeda2d2d46c402ac5b977b2d2317
tsinatwon <tsinat2wondimu@gmail.com>
2022-07-27 10:03:56 +0000

    valdication

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  squae class"""
+
+
+class Square:
+    """class square with attribute size"""
+
+    def __init__(self, size=0):
+        """ intialize the square class"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
