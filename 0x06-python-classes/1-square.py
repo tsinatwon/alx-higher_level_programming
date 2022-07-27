b95d396377d3897f8263a43d41a7e39f1889249e
tsinatwon <tsinat2wondimu@gmail.com>
2022-07-27 09:42:01 +0000

    class with attributr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" square with private attribute"""
+
+
+class Square:
+    """ class with private  attribute"""
+
+    def __init__(self, size):
+        """intialize new square"""
+
+        self.__size = size
